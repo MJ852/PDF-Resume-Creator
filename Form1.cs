9b5b8648781366504e1ea85060bbc73d921bc172
MJ852 <maryjoycecalangian852@gmail.com>
2022-07-11 20:16:44 +0800

    Added pdf code, but not totally functioning

--- Form1.cs
+++ Form1.cs
@@ -24,9 +24,6 @@ namespace PDF_Creator
         private string name, position, objective, email, contactno, address, age, educational,
             college, graduated, work1, company1, yearwork1, work2, company2, yearwork2, skill1, skill2, skill3;
 
-
-
-        private bool generate = false;
         private bool readjson = false;
         public Form1()
         {
@@ -80,34 +77,92 @@ namespace PDF_Creator
         {
             if (readjson)
             {
-                FileStream destination = new FileStream(@"C:\Users\Joyce Calangian\Documents\MJ\PUP\SUBJECTS\OBJECT ORIENTED PROGRAMMING\PDF Creator" + name + ".pdf", FileMode.Create);
+                string jsonFromFile;
+                string openfile = File.ReadAllText(_path);
+                jsonFromFile = File.ReadAllText(_path);
+                Resume myResume = JsonConvert.DeserializeObject<Resume>(jsonFromFile);
+                FileStream destination = new FileStream(@"C:\Users\Joyce Calangian\Documents\MJ\PUP\SUBJECTS\OBJECT ORIENTED PROGRAMMING\PDF Creator\CALANGIAN_MARYJOYCE.pdf", FileMode.Create);
 
                 BaseFont bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-                iTextSharp.text.Font biggest = new iTextSharp.text.Font(bfTimes, 34, iTextSharp.text.Font.BOLD);
-                iTextSharp.text.Font big = new iTextSharp.text.Font(bfTimes, 22, iTextSharp.text.Font.BOLD);
-                iTextSharp.text.Font normal = new iTextSharp.text.Font(bfTimes, 16, iTextSharp.text.Font.NORMAL);
-                iTextSharp.text.Font small = new iTextSharp.text.Font(bfTimes, 14, iTextSharp.text.Font.NORMAL);
+                iTextSharp.text.Font biggest = new iTextSharp.text.Font(bfTimes, 34, iTextSharp.text.Font.BOLD); //name
+                iTextSharp.text.Font big = new iTextSharp.text.Font(bfTimes, 22, iTextSharp.text.Font.BOLD); //section title
+                iTextSharp.text.Font normal = new iTextSharp.text.Font(bfTimes, 16, iTextSharp.text.Font.NORMAL); //title
+                iTextSharp.text.Font small = new iTextSharp.text.Font(bfTimes, 14, iTextSharp.text.Font.NORMAL); // details
 
                 Document doc = new Document(PageSize.A4, 60, 75, 60, 75);
                 PdfWriter writer = PdfWriter.GetInstance(doc, destination);
-               
 
+                doc.Open();
+
+                Paragraph paragraph = new Paragraph();
+
+                paragraph.Add(new Phrase(name + "\n" + "\n", biggest));
+                paragraph.Add(new Phrase(position, big));
+                //line 
+                paragraph.Add(new Phrase(objective + "\n" + "\n", big));
+
+                //Personal Information
+                paragraph.Add(new Phrase(objective + "\n" + "\n", small));
+                paragraph.Add(new Phrase(email + "\n" + "\n", small));
+                paragraph.Add(new Phrase(contactno + "\n" + "\n", small));
+                paragraph.Add(new Phrase(address + "\n" + "\n", small));
+                paragraph.Add(new Phrase(age + "\n" + "\n", small));
+
+                //Education
+
+                paragraph.Add(new Phrase(educational  +"\n" + "\n", big));
+                paragraph.Add(new Phrase(college + "\n" + "\n", small));
+                paragraph.Add(new Phrase(graduated + "\n" + "\n", small));
+
+                //Work Experience
+
+                paragraph.Add(new Phrase(work1 + "\n" + "\n", big));
+                paragraph.Add(new Phrase("• " + company1 + "\n" + "\n", small));
+                paragraph.Add(new Phrase("• " +yearwork1 + "\n" + "\n", small));
+                paragraph.Add(new Phrase(work2 + "\n" + "\n", big));
+                paragraph.Add(new Phrase("• " + company2 + "\n" + "\n", small));
+                paragraph.Add(new Phrase("• " + yearwork2 + "\n" + "\n", small));
+
+                //Skills
+                paragraph.Add(new Phrase("• " + skill1 + "\n" + "\n", small));
+                paragraph.Add(new Phrase("• " + skill2 + "\n" + "\n", small));
+                paragraph.Add(new Phrase("• " + skill3 + "\n" + "\n", small));
+
+
+                doc.Add(paragraph);
+                doc.Close();
+                writer.Close();
+                destination.Close();
+                readjson = false;
+
+                MessageBox.Show("Your PDF file has been saved!");
+            }
+            else
+            {
+                MessageBox.Show("Error");
             }
         }
 
         class Resume
         {
-            public string lastname { get; set; }
-            public string firstname { get; set; }
-            public string job { get; set; }
+            public string name { get; set; }
+            public string position { get; set; }
+            public string objective { get; set; }
             public string email { get; set; }
-            public string contact { get; set; }
+            public string contactno { get; set; }
             public string address { get; set; }
-            public string aboutme { get; set; }
-            public string WorkExp { get; set; }
-            public string skills { get; set; }
-            public string interests { get; set; }
-
+            public string age { get; set; }
+            public string educational { get; set; }
+            public string college { get; set; }
+            public string graduated { get; set; }
+            public string work1 { get; set; }
+            public string company1 { get; set; }
+            public string yearwork1 { get; set; }
+            public string company2 { get; set; }
+            public string yearwork2{ get; set; }
+            public string skill1 { get; set; }
+            public string skill2 { get; set; }
+            public string skill3 { get; set; }
 
 
 
